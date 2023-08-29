8db090cf510c771e58dc447b4a0213df096af3bf
Nalini Ajwani <98285552+nalini-ajwani@users.noreply.github.com>
2023-08-29 16:42:19 +0530

    Time: 15 ms (80.00%), Space: 13.4 MB (60.00%) - LeetHub

---
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def peopleAwareOfSecret(self, n, delay, forget):
+        dp = [1] + [0] * (n - 1)
+        mod = 10**9 + 7
+        share= 0 
+        for i in range(1,n):
+            dp[i] = share = (share + dp[i - delay] - dp[i - forget]) % mod
+        return sum(dp[-forget:]) % mod
