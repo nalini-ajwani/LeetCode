edb6f14a0037ea2a4c14ad974702f90643f8a736
Nalini Ajwani <98285552+nalini-ajwani@users.noreply.github.com>
2023-10-08 13:17:18 +0530

    Added solution - LeetHub

---
+++ Get minimum element from stack - GFG/get-minimum-element-from-stack.py
@@ -0,0 +1,59 @@
+#User function Template for python3
+
+class stack:
+    # def __init__(self):
+    #     self.s=[]
+    #     self.minEle=[]
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, x):
+        self.stack.append(x)
+        if not self.min_stack or x <= self.min_stack[-1]:
+            self.min_stack.append(x)
+
+    def pop(self):
+        if not self.stack:
+            return -1
+        if self.stack[-1] == self.min_stack[-1]:
+            self.min_stack.pop()
+        return self.stack.pop()
+
+    def getMin(self):
+        if not self.min_stack:
+            return -1
+        return self.min_stack[-1]
+
+
+#{ 
+ # Driver Code Starts
+#contributed by RavinderSinghPB
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        q = int(input())
+
+        arr = [int(x) for x in input().split()]
+
+        stk=stack()  
+
+        qi = 0
+        qn=1
+        while qn <= q:
+            qt = arr[qi]
+
+            if qt == 1:
+                stk.push(arr[qi+1])
+                qi+=2
+            elif qt==2:
+                print(stk.pop(),end=' ')
+                qi+=1
+            else:
+                print(stk.getMin(),end=' ')
+                qi+=1
+            qn+=1
+        print()
+
+# } Driver Code Ends
