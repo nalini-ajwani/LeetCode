0c7c991c0a3acc37b8996cfb752173b08911fbbf
Nalini Ajwani <98285552+nalini-ajwani@users.noreply.github.com>
2023-11-29 23:39:19 +0530

    Added solution - LeetHub

--- Knight Walk - GFG/knight-walk.py
+++ Knight Walk - GFG/knight-walk.py
@@ -6,16 +6,12 @@ class Solution:
             (-2, 1), (-1, 2), (1, 2), (2, 1)
         ]
 
-        # Convert positions to 0-based indexing
         start = (KnightPos[0] - 1, KnightPos[1] - 1)
         target = (TargetPos[0] - 1, TargetPos[1] - 1)
 
-        # Initialize a visited array to keep track of visited positions
         visited = [[False] * N for _ in range(N)]
 
-        # Initialize the queue for BFS
-        queue =
-        deque([(start, 0)])  # (position, steps)
+        queue = deque([(start, 0)])  
         visited[start[0]][start[1]] = True
 
         while queue:
@@ -24,16 +20,13 @@ class Solution:
             if current == target:
                 return steps
 
-            # Explore possible moves
             for move in moves:
                 new_x, new_y = current[0] + move[0], current[1] + move[1]
 
-                # Check if the new position is valid and not visited
                 if 0 <= new_x < N and 0 <= new_y < N and not visited[new_x][new_y]:
                     queue.append(((new_x, new_y), steps + 1))
                     visited[new_x][new_y] = True
 
-        # If the target cannot be reached
         return -1
 
 
