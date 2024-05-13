a4750e7c60832874d707e62f27f6182311cfb1bb
Nalini Ajwani <98285552+nalini-ajwani@users.noreply.github.com>
2024-05-13 23:55:45 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Pairwise swap elements of a linked list/pairwise-swap-elements-of-a-linked-list.py
@@ -0,0 +1,101 @@
+"""  list Node is as defined below:
+
+class Node:
+    def __init__(self, data):
+		self.data = data
+		self.next = None
+
+"""
+
+# complete this function
+# return head of list after swapping
+class Solution:    
+    def pairWiseSwap(self, head):
+        # code here
+        if head is None or head.next is None:
+            return head
+        
+        # Initialize previous and current pointers
+        prev = head
+        current = head.next
+        
+        # Change head to the second node
+        head = current
+        
+        # Traverse the linked list and swap nodes pairwise
+        while True:
+            next_ = current.next
+            current.next = prev  # Swap the nodes
+            if next_ is None or next_.next is None:
+                prev.next = next_
+                break
+            # Connect the previous node to the next pair
+            prev.next = next_.next
+            # Move to the next pair
+            prev = next_
+            current = prev.next
+            
+        return head
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def insert(self,val):
+        if self.head is None:
+            self.head = Node(val)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(val)
+            self.tail = self.tail.next
+
+def printList(n):
+    while n:
+        print(n.data, end=' ')
+        n = n.next
+    print()
+
+if __name__=="__main__":
+    for _ in range(int(input())):
+        n = int(input())
+        arr = [int(x) for x in input().split()]
+        
+        lis = LinkedList()
+        for i in arr:
+            lis.insert(i)
+            
+        dict1 = {}
+        temp = lis.head
+        while temp:
+            dict1[temp] = temp.data
+            temp = temp.next
+        
+        failure = LinkedList()
+        failure.insert(-1)
+        
+        head = Solution().pairWiseSwap(lis.head)
+        
+        temp = head
+        f = 0
+        while temp:
+            if dict1[temp] != temp.data:
+                f = 1;
+            temp = temp.next
+        
+        if f:
+            printList(failure)
+        else:
+            printList(head)
+
+# } Driver Code Ends
