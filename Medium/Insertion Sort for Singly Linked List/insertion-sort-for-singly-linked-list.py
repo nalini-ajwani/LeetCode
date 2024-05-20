af6f183a6c6eac0f1212f4b5e50a71cbb6f107cd
Nalini Ajwani <98285552+nalini-ajwani@users.noreply.github.com>
2024-05-20 15:56:12 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Insertion Sort for Singly Linked List/insertion-sort-for-singly-linked-list.py
@@ -0,0 +1,85 @@
+#User function Template for python3
+
+'''
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+'''
+
+class Solution:
+    def insertionSort(self, head):
+        #code here
+        sorted_head = None
+        cur = head
+        while cur is not None:
+            nn = cur.next
+            sorted_head = self.sortedInsert(sorted_head, cur)
+            cur = nn
+        return sorted_head
+        
+    def sortedInsert(self, sorted_head, nn):
+        if sorted_head is None or sorted_head.data >= nn.data:
+            nn.next = sorted_head
+            sorted_head = nn
+        
+        else:
+            cur = sorted_head
+            while cur.next is not None and cur.next.data < nn.data:
+                cur = cur.next
+            nn.next = cur.next
+            cur.next = nn
+        return sorted_head
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+    
+# Node Class
+INF = float("inf")
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+def printList(head):
+    if head is None:
+        print(' ')
+        return
+    curr_node = head
+    while curr_node:
+        print(curr_node.data,end=" ")
+        curr_node=curr_node.next
+    print(' ')
+    
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        n = int(input())
+        a = Node(INF)
+        nodes = list(map(int, input().strip().split()))
+        ptr = a
+        for x in nodes:
+            ptr.next = Node(INF)
+            ptr = ptr.next
+            ptr.data = x
+        a = a.next
+        ob=Solution()
+        head = ob.insertionSort(a)
+        printList(head)
+# } Driver Code Ends
