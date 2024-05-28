067aca6852417ce8aa5f0b93ed2691b78650de53
Nalini Ajwani <98285552+nalini-ajwani@users.noreply.github.com>
2024-05-28 17:13:52 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Add 1 to a number represented as linked list/add-1-to-a-number-represented-as-linked-list.py
@@ -0,0 +1,88 @@
+#User function Template for python3
+
+'''
+
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+'''
+
+class Solution:
+    def addOne(self,head):
+        #Returns new head of linked List.
+        def rev(head):
+            prev = None
+            curr = head
+            while curr:
+                nn = curr.next
+                curr.next = prev
+                prev = curr
+                curr = nn
+            return prev
+        head = rev(head)
+        
+        curr = head
+        carry = 1
+        while curr:
+            curr.data += carry
+            if curr.data < 10:
+                carry = 0
+                break
+            else:
+                curr.data = 0
+                carry = 1
+                
+            if curr.next is None and carry:
+                curr.next = Node(0)
+            curr = curr.next
+        head = rev(head)
+        return head
+                
+            
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # creates a new node with given value and appends it at the end of the linked list
+    def insert(self, value):
+        if self.head is None:
+            self.head = Node(value)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(value)
+            self.tail = self.tail.next
+
+def PrintList(head):
+    while head:
+        print(head.data,end='')
+        head = head.next
+
+if __name__ == '__main__':
+    t=int(input())
+    for _ in range(t):
+        
+        num = input()
+        ll = LinkedList() # create a new linked list 'll1'.
+        for digit in num:
+            ll.insert(int(digit))  # add to the end of the list
+        
+        resHead = Solution().addOne(ll.head)
+        PrintList(resHead)
+        print()
+
+
+# } Driver Code Ends
