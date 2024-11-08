825577d702700e3036738bffa4a108fc5428698b
Nalini Ajwani <98285552+nalini-ajwani@users.noreply.github.com>
2024-11-08 11:52:40 +0530

    Time: 25 ms (19.88%), Space: 14 MB (6.33%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,4 +5,14 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        return Counter(s) == Counter(t)
+        if len(s) != len(t):
+            return False
+        
+        cntS, cntT = {}, {}
+        for i in range(len(s)):
+            cntS[s[i]] = 1 + cntS.get(s[i], 0)
+            cntT[t[i]] = 1 + cntT.get(t[i], 0)
+        for c in cntS:
+            if cntS[c] != cntT.get(c, 0):
+                return False
+        return True
