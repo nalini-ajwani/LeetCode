906912ece13f6c9b1d7745fa0aa79933dadbe2c5
Nalini Ajwani <98285552+nalini-ajwani@users.noreply.github.com>
2025-07-04 12:55:09 +0530

    Time: 0 ms (100%), Space: 17.9 MB (65.48%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,17 +1,11 @@
-class Solution(object):
-    def nextGreaterElement(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: List[int]
-        """
-        nextg = {}
-        stk = []
-        
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        next_greater = {}
+        s = []
+
         for num in reversed(nums2):
-            while stk and stk[-1] <= num:
-                stk.pop()
-            nextg[num] = stk[-1] if stk else -1
-            stk.append(num)
-            
-        return [nextg[num] for num in nums1]
+            while s and s[-1] <= num:
+                s.pop()
+            next_greater[num] = s[-1] if s else -1
+            s.append(num)
+        return [next_greater[n] for n in nums1]
